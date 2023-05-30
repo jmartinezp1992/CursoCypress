2962ab0f9620895e62f8fc853914375bc6311c8d
Juan Martínez <jmartinezp1992@gmail.com>
2023-05-30 18:19:45 +0200

    Subida fichero apuntes cypress

---
+++ cypress/e2e/Pruebas/EjemplosPracticos.cy.js
@@ -0,0 +1,19 @@
+describe('Loguear - Basic Auth y Auth con Forms', ()=>{
+    it('Sin loguear',()=>{
+         cy.visit('https://the-internet.herokuapp.com/basic_auth')
+         cy.get('p').should('include.text','Congratulations')
+    })
+
+
+    it('Loguea usando auth de Cypress',()=>{
+        cy.visit('https://the-internet.herokuapp.com/basic_auth',{
+            auth:{
+                username:'admin',
+                password:'admin'
+            }
+        })
+        cy.get('p').should('include.text','Congratulations')
+
+    })
+
+})
